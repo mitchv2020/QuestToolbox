cac40ee5941d99ed6d1f64f160db883ddeb88fcc
mitchv2020 <70819540+mitchv2020@users.noreply.github.com>
2021-10-14 18:04:19 +0100

    Add files via upload

---
+++ Requirements/wiredalvr.bat
@@ -0,0 +1,10 @@
+@echo off
+mode con: cols=65 lines=16 
+echo Starting wired ALVR...
+adb forward tcp:9943 tcp:9943
+adb forward tcp:9944 tcp:9944
+:warning
+cls
+echo [41mDo NOT close this window or wired ALVR will stop working.[0m
+pause
+goto warning
