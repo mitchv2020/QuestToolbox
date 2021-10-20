442e2888e05ff06e1edc5d15e26058620c5c9d1f
mitchv2020 <70819540+mitchv2020@users.noreply.github.com>
2021-10-20 18:20:43 +0100

    Add files via upload

--- Requirements/keepalive.bat
+++ Requirements/keepalive.bat
@@ -10,23 +10,31 @@ echo ==========================================
 
 cmdMenuSel f870 "Enable Keep Alive" "Disable Keep Alive" "==Exit=="
 
-if "%ERRORLEVEL%"=="1" (
+if "%errorlevel%"=="1" (
 cls
 echo Enabling...
 adb shell svc power stayon true
+if "%errorlevel%"=="1" goto error
 echo Enabled!
 pause
 goto start
 )
 
-if "%ERRORLEVEL%"=="2" (
+if "%errorlevel%"=="2" (
 cls
 echo Disabling...
 adb shell svc power stayon false
+if "%errorlevel%"=="2" goto error
 echo Disabled!
 pause
 goto start
 )
 
-if "%ERRORLEVEL%"=="3" exit
+if "%errorlevel%"=="3" exit
 
+:error
+cls
+echo [7mYou have either more than 1 or no Android devices connected!
+echo Please disconnect any other devices or connect your Quest.[0m
+pause
+goto start
--- Requirements/packages.bat
+++ Requirements/packages.bat
@@ -5,5 +5,14 @@ echo ==========================================
 echo List of packages: 
 echo ==========================================
 adb shell pm list packages -3
+
+if "%errorlevel%"=="-1" (
+cls
+echo [7mYou have either more than 1 or no Android devices connected!
+echo Please disconnect any other devices or connect your Quest.[0m
+pause
+goto start
+)
+
 pause
 goto start
