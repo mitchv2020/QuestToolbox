b19de0c5adfd9e57713be26a12f0ecfbc064d5d5
mitchv2020 <70819540+mitchv2020@users.noreply.github.com>
2021-11-13 21:41:32 +0000

    Add files via upload

---
+++ Requirements/keepaliveReplay.bat
@@ -0,0 +1,23 @@
+echo off
+:: Sets the title and window size respectivly
+title KeepAlive
+mode con: cols=72 lines=20 
+
+:tap
+cls
+echo Please keep this window open to keep the Quest from sleeping
+echo [7mPinging the device! Keep the device connected and this window open![0m
+adb shell input tap 916 960
+
+if "%errorlevel%"=="-1" (
+cls
+echo [7mYou have either more than 1 or no Android devices connected!
+echo Please disconnect any other devices or connect your Quest.[0m
+pause
+goto tap
+)
+
+TIMEOUT 5 
+goto tap
+
+exit
